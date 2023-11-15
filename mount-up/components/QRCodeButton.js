04a67c3905cdc1ee8cd0217b0cdec65e55fde36f
WizoftheWeb <146019633+WizoftheWeb@users.noreply.github.com>
2023-11-15 14:48:56 -0500

    Update QRCodeButton.js

--- mount-up/components/QRCodeButton.js
+++ mount-up/components/QRCodeButton.js
@@ -1,11 +1,23 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import {Ionicons} from 'react-native-vector-icons';
 
-function QRCodeButton(){
+export default function QRCodeButton({navigation}){
+    function onClick(){
+        navigation.navigate('QRCode');
+    }
+
     return(
         <View>
-
+            <TouchableOpacity containerStyle={buttonStyles.buttonQR} onPress={() => onClick()}>
+                <Ionicons name="qrcode" size={30} color={"#003366"}/>
+            </TouchableOpacity>
         </View>
     )
-}
+};
 
-export default QRCodeButton;
+const buttonStyles = StyleSheet.create({
+    buttonQR: {
+        backgroundColor: "#FFCC00"
+    },
+});
